723088976560a0ea3500ad2dfffd99647119feb7
JoostGosens <61309868+JoostGosens@users.noreply.github.com>
2020-03-06 11:21:58 +0100

    update

--- app/src/main/java/com/example/bp3/MainActivity.java
+++ app/src/main/java/com/example/bp3/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bp3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button Menu_button = findViewById(R.id.Menu_button);
     }
 }
