cebb2ef6244996a4e2fafe02fb5f778283cd740b
jouckeb <56512296+jouckeb@users.noreply.github.com>
2020-03-19 13:31:32 +0100

    first commit

--- app/src/main/java/com/example/bp3/MainActivity.java
+++ app/src/main/java/com/example/bp3/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.bp3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.bp3.Menuscherm;
+import com.example.bp3.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void naarMenu(View v) {
+        Intent intent = new Intent(this, Menuscherm.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/bp3/Menuscherm.java
@@ -0,0 +1,16 @@
+package com.example.bp3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bp3.R;
+
+public class Menuscherm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menuscherm);
+    }
+}
