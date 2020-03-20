f23f38aebc40eec1e750b87c516ecec3b341dd5f
usersomeoneR <61309701+usersomeoneR@users.noreply.github.com>
2020-03-20 10:34:12 +0100

    Checken

--- app/src/main/java/com/example/bp3/MainActivity.java
+++ app/src/main/java/com/example/bp3/MainActivity.java
@@ -9,24 +9,46 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private RequestQueue mQueue;
+    private Button btnNotificationn;
+    private TextView mTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Moet het nog ergens inzetten.
-
+        btnNotificationn = findViewById(R.id.btnNotification);
+        mTextView = findViewById(R.id.textView);
         mQueue = Volley.newRequestQueue(this);
-        jsonParse();
+
+        btnNotificationn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                jsonParse();
+
+            }
+        });
+
+
+
 
         Intent notifyIntent = new Intent(this,MyReceiver.class);
         //Stuur iedere 24 uur een notificatie over de stap.
@@ -37,8 +59,37 @@ public class MainActivity extends AppCompatActivity {
                 86400000, pendingIntent);
     }
 
+
     private void jsonParse(){
         //De huidige gebruiker is een tester: dit is standaard 1234567
-        String url = "";
+        String url = "https://adaonboarding.ml/t1/Studentstap.php?studentnummer=1234567";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>(){
+                    @Override
+                    public void onResponse(JSONObject response){
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("rij");
+                            for (int i = 0; i < jsonArray.length(); i++){
+                                JSONObject rij = jsonArray.getJSONObject(i);
+
+                                int id = rij.getInt("id");
+                                int studentnummer = rij.getInt("studentnummer");
+                                int stapnu = rij.getInt("stapnu");
+
+                                mTextView.append("ID: " + String.valueOf(id) + ", Studentnummer: " + String.valueOf(studentnummer) + ", Stapnu: " + String.valueOf(stapnu) + "\n\n");
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error){
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
     }
 }
