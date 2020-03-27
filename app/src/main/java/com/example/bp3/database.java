75366ad4679606bcaf5ff90de85dde482a933f47
jouckeb <56512296+jouckeb@users.noreply.github.com>
2020-03-27 10:06:52 +0100

    commit 3, connectie geschreven, niet werkend

--- app/src/main/java/com/example/bp3/database.java
+++ app/src/main/java/com/example/bp3/database.java
@@ -1,4 +1,36 @@
-package com.example.bp3;
-
-public class database {
-}
+//package com.example.bp3;
+//
+//private void jsonParse(){
+//        //De huidige gebruiker is een tester: dit is standaard 1234567
+//        String url="";
+//        String url="https://adaonboarding.ml/t1/Studentstap.php?studentnummer=1234567";
+//        JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET,url,null,
+//        new Response.Listener<JSONObject>(){
+//@Override
+//public void onResponse(JSONObject response){
+//        try{
+//        JSONArray jsonArray=response.getJSONArray("rij");
+//        for(int i=0;i<jsonArray.length();i++){
+//        JSONObject rij=jsonArray.getJSONObject(i);
+//
+//        int id=rij.getInt("id");
+//        int studentnummer=rij.getInt("studentnummer");
+//        int stapnu=rij.getInt("stapnu");
+//
+//        mTextView.append("ID: "+String.valueOf(id)+", Studentnummer: "+String.valueOf(studentnummer)+", Stapnu: "+String.valueOf(stapnu)+"\n\n");
+//
+//        }
+//        }catch(JSONException e){
+//        e.printStackTrace();
+//        }
+//
+//        }
+//        },new Response.ErrorListener(){
+//@Override
+//public void onErrorResponse(VolleyError error){
+//        error.printStackTrace();
+//        }
+//        });
+//        mQueue.add(request);
+//        }
+//        }
