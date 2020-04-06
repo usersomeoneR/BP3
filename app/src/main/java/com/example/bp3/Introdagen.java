cddba3ffd214a23c3812a2a71c61a7a40bcf1f5f
jouckeb <56512296+jouckeb@users.noreply.github.com>
2020-04-06 13:46:10 +0200

    commit 1, klaar

--- app/src/main/java/com/example/bp3/Introdagen.java
+++ app/src/main/java/com/example/bp3/Introdagen.java
@@ -3,14 +3,12 @@ package com.example.bp3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Introdagen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_introdagen);
     }
 }
